def9deb3f3fafaef8ad94dd8f2cc6d8e570cec13
Harish Sanjaykumar Pukale <46403164+harishsanjaykumarpukale@users.noreply.github.com>
2019-12-26 19:12:06 +0530

    Create program.c

---
+++ Sample Programs/newInt/program.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include"gfl.h"
+void main(){
+
+
+        mpint x;
+
+
+
+        x=newInt();
+
+        if(zeroint(x))
+                printf("\nIt's zero element.\n");
+        else
+                printf("\nIt's not a zero element.\n");
+}
