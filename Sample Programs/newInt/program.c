c195fee36dc3f05cfb5c20a5014d935a92de8c4e
Harish Sanjaykumar Pukale <46403164+harishsanjaykumarpukale@users.noreply.github.com>
2019-12-27 13:15:04 +0530

    Update program.c

--- Sample Programs/newInt/program.c
+++ Sample Programs/newInt/program.c
@@ -14,3 +14,30 @@ void main(){
         else
                 printf("\nIt's not a zero element.\n");
 }
+
+
+//Function definition
+//Present in gfl.h
+/*
+NODE insertbegin(NODE start,long int item){
+  NODE temp= (NODE) malloc (sizeof(struct node));
+  temp->data=item;
+  temp->addr=NULL;
+  if (start==NULL)
+       return temp;
+  temp->addr=start;
+  return temp;
+}
+
+mpint newInt(){ // Initializes a mpint to zero element
+	// This function must be called to initialize the variable before any use
+	mpint x;
+
+
+	x.sign=' ';
+	x.size=1;
+	x.start=insertbegin(x.start,0);
+
+	return x;
+}
+*/
